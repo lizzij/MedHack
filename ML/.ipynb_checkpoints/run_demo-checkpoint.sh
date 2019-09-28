1f7531c6a0a91055269f43aa08a7eacf4f24d961
Jay Mody <jaykmody@gmail.com>
2019-09-28 14:36:30 +0000

    small fixes and removed bin file

---
+++ ML/.ipynb_checkpoints/run_demo-checkpoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+OPENPOSE_DIR="/home/start/openpose/"
+BIN_FPATH="/home/start/openpose/build/examples/openpose/openpose.bin"
+DEMO_DIR="/home/start/MedHack/ML/demo/"
+CUR_DIR="/home/start/MedHack/ML/"
+
+cd $OPENPOSE_DIR
+$BIN_FPATH --image_dir $DEMO_DIR --display 0 --render_pose 0 --write_json $DEMO_DIR
+
+cd $CUR_DIR
+python demo.py
+echo done_executing
