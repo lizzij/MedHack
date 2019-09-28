c026f245cfc3a8500bb3943c308ae8cf177e9474
Jay Mody <jaykmody@gmail.com>
2019-09-28 20:20:18 +0000

    cleanup and setup

--- ML/app.py
+++ ML/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify, Response
 
 from pose import process
 from bp import validate
+from ocr import get_bp
 
 app = Flask(__name__)
 
@@ -18,5 +19,18 @@ def entry():
     response = validate(keypoints)
     
     response["keypoints"] = [list(trio) for trio in keypoints]
+
+    return jsonify(response)
+
+@app.route("/ocr", methods=['POST'])
+def ocr():
+    imgarr = np.fromstring(request.data, np.uint8)
+    img = cv2.imdecode(imgarr, cv2.IMREAD_COLOR)
+
+    blood_pressure = get_bp(img)
+    
+    response {
+        "blood_pressure": blood_pressure
+    }
 
     return jsonify(response)
