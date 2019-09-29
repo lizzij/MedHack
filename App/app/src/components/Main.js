eb5808f929a8c86746ffec6a2fbc3e1d14e4a491
eliza <elizazijinhuang@gmail.com>
2019-09-28 23:28:16 -0400

    icon

--- App/app/src/components/Main.js
+++ App/app/src/components/Main.js
@@ -1,6 +1,6 @@
 'use strict';
 import React, { PureComponent } from 'react';
-import { AppRegistry, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { AppRegistry, StyleSheet, TouchableOpacity, View, Image } from 'react-native';
 import { mapping, light as lightTheme } from '@eva-design/eva';
 import { ApplicationProvider, Button, Layout, Text } from 'react-native-ui-kitten';
 import { RNCamera } from 'react-native-camera';
@@ -8,6 +8,7 @@ import { CameraScreen } from '../components/CameraScreen.js';
 
 export const Main = () => (
   <Layout style={styles.container}>
+    <Image style={styles.logo} source={'../img/icon3.png'} />
     <Text style={styles.text} category='h4'>Welcome to myBP</Text>
     <View style={styles.cameraWrapper}>
       <CameraScreen />
@@ -16,6 +17,7 @@ export const Main = () => (
 );
 
 const styles = StyleSheet.create({
+  logo: { zIndex: 2 },
   container: { flex: 1, alignItems: 'center', paddingTop: 0, backgroundColor: 'transparent'},
   text: { color: 'white', position: 'absolute', top: 100, zIndex: 2 },
   cameraWrapper: { width: '100%', height: '100%', zIndex: 1 },
